400d60ff3c11f782e7ef27d78fa55350d099cd0f
Stubbleman <q19950129@gmail.com>
2017-01-03 01:08:06 +0800

    Form1
    Bezier 連連看

--- numm/Form1.Designer.cs
+++ numm/Form1.Designer.cs
@@ -32,24 +32,28 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(502, 12);
+            this.button1.Location = new System.Drawing.Point(1088, 24);
+            this.button1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(163, 46);
             this.button1.TabIndex = 1;
             this.button1.Text = "Clear";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 24);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(483, 424);
+            this.pictureBox1.Size = new System.Drawing.Size(1047, 848);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
@@ -57,22 +61,32 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(522, 108);
+            this.label1.Location = new System.Drawing.Point(1120, 206);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(15, 12);
+            this.label1.Size = new System.Drawing.Size(90, 24);
             this.label1.TabIndex = 3;
-            this.label1.Text = "sd";
+            this.label1.Text = "Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1216, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(692, 448);
+            this.ClientSize = new System.Drawing.Size(1499, 896);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.Name = "Form1";
             this.Text = "numm-Bézier curve generator";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -85,6 +99,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- numm/Form1.cs
+++ numm/Form1.cs
@@ -9,14 +9,18 @@ using System.Windows.Forms;
 
 namespace numm
 {
-    
+
 
     public partial class Form1 : Form
     {
 
-        Bezier B = new Bezier();
         Graphics G;
-
+        List<PointF> Pos_list = new List<PointF>();
+        List<PointF> Point_Bezier = new List<PointF>();
+        Bezier B = new Bezier();
+        float h = 0.01f; // Resolution of each point
+        int count = 0;
+               
         public Form1()
         {
             InitializeComponent();
@@ -29,70 +33,129 @@ namespace numm
             switch (e.Button)
             {
                 case MouseButtons.Left:
-
-                    B.addP(new Point(e.X, e.Y));
-                    
-                    Draw();
-                    label1.Text = B.listP();
-                    
-
+                    Pen pen_dot = new Pen(Color.Red);
+                    G.DrawRectangle(pen_dot, e.X, e.Y, 1, 1);
+                    PointF Pos_mouse = new PointF(e.X, e.Y);
+                    Pos_list.Add(Pos_mouse);
+                    label2.Text = Convert.ToString(Pos_list.Count);
+                    count++;
                     break;
                 case MouseButtons.Right:
-                    
+                    //Line_spline(Pos_list);
+                    Bezier_curve();
                     break;
             }
         }
 
-
-        void Draw()
+        private void Line_spline(List<PointF> points)
         {
-            foreach(Point p in B.CtrlPoint)
-            {
-                System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
-                G.FillEllipse( myBrush, new Rectangle(p,new Size(5,5)) );
-               
-            }
+            Pen pen_line = new Pen(Color.Blue);
+            for (int i = 0; i < (points.Count - 1); i++)
+                G.DrawLine(pen_line, points[i], points[i + 1]);
         }
 
-        
+        private void Bezier_curve()  // Draw the Bezier curve 
+        {
+            Bezier_calculate();            
+            Line_spline(Point_Bezier);
+        }
+
+        private void Bezier_calculate() // Calculate each point
+        {
+            PointF[,] Stack = new PointF[Pos_list.Count, Pos_list.Count];
+            for (int i = 0; i < Pos_list.Count; i++)
+            {
+                Stack[i, 0] = Pos_list[i];
+            }
+
+
+            for (float u = 0; u <= 1; u = u + h)
+            {
+                for (int j = 0; j < (Pos_list.Count-1); j++)
+                {
+                    for (int i = 0; i < (Pos_list.Count - 1); i++)
+                        Stack[i, j + 1] = new PointF((1 - u) * Stack[i + 1, j].X + u * Stack[i, j].X, (1 - u) * Stack[i + 1, j].Y + u * Stack[i, j].Y);
+                }
+                Point_Bezier.Add(Stack[0, (Pos_list.Count - 1)]);
+                //label1.Text = Convert.ToString(Stack[0, (Pos_list.Count - 1)]);
+                //label1.Text = Convert.ToString(h);
+            }
+        }
+          
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            G.Clear(Color.White);
+            Pos_list.Clear();
+            Point_Bezier.Clear();
+            label2.Text = Convert.ToString(Pos_list.Count);
+            count = 0;           
+        }
+
+        //public struct PointD  //因為Point宣告是int，只好自己定義double
+        //{
+        //    public double X;
+        //    public double Y;
+
+        //    public PointD(double x, double y)
+        //    {
+        //        X = x;
+        //        Y = y;
+        //    }
+
+        //    public Point ToPoint()
+        //    {
+        //        return new Point((int)X, (int)Y);
+        //    }
+
+        //    public override bool Equals(object obj)
+        //    {
+        //        return obj is PointD && this == (PointD)obj;
+        //    }
+        //    public override int GetHashCode()
+        //    {
+        //        return X.GetHashCode() ^ Y.GetHashCode();
+        //    }
+        //    public static bool operator ==(PointD a, PointD b)
+        //    {
+        //        return a.X == b.X && a.Y == b.Y;
+        //    }
+        //    public static bool operator !=(PointD a, PointD b)
+        //    {
+        //        return !(a == b);
+        //    }
+        //}
+
     }
+
+    
 
     public class Bezier
     {
-        public List<Point> CtrlPoint = new List<Point>();
+        public Point[] CtrlPoint;
+
 
         public Bezier()
-        {
-
+        {           
+                
         }
+
+  
 
         public Point P(double t)
         {
             Point p = new  Point(0, 0);
 
             // 點賦值方法
+            
             p.X = 10;
             p.Y = 20;
             // 另一點賦值方法
             p = new Point(30, 50);
 
-           
+            
+
             return p;
-        }
-
-        public void addP(Point a)
-        {                        
-            CtrlPoint.Add( a);
-        }
-
-        public string listP()
-        {
-            string s = "";
-            foreach (Point p in CtrlPoint)
-            {
-                s += p.ToString() + '\n';
-            }
-            return s;
         }
     }
 }
