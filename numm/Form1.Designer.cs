e591e782a7dd8b66ab9e0304fc8aabcbdbdab0bb
david11014 <david11014@gmail.com>
2017-01-03 13:33:44 +0800

    muti curve

--- numm/Form1.Designer.cs
+++ numm/Form1.Designer.cs
@@ -29,26 +29,22 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.ClearButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ClearCurve1Btn = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ClearCurve2Btn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
-            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // ClearButton
-            // 
-            this.ClearButton.Location = new System.Drawing.Point(3, 3);
-            this.ClearButton.Name = "ClearButton";
-            this.ClearButton.Size = new System.Drawing.Size(75, 23);
-            this.ClearButton.TabIndex = 1;
-            this.ClearButton.Text = "Clear";
-            this.ClearButton.UseVisualStyleBackColor = true;
-            this.ClearButton.Click += new System.EventHandler(this.Clear);
             // 
             // pictureBox1
             // 
@@ -62,16 +58,6 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1, 47);
-            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 12);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Number:";
             // 
             // menuStrip1
             // 
@@ -88,26 +74,90 @@
             this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
             this.clearToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
             this.clearToolStripMenuItem.Text = "Clear";
-            this.clearToolStripMenuItem.Click += new System.EventHandler(this.Clear);
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.ClearCurve1);
             // 
-            // panel1
+            // tabControl1
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.ClearButton);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(490, 28);
-            this.panel1.MaximumSize = new System.Drawing.Size(200, 400);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 400);
-            this.panel1.TabIndex = 5;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(489, 27);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(206, 324);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.ClearCurve1Btn);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(198, 298);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Curve1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number:";
+            // 
+            // ClearCurve1Btn
+            // 
+            this.ClearCurve1Btn.Location = new System.Drawing.Point(3, 5);
+            this.ClearCurve1Btn.Name = "ClearCurve1Btn";
+            this.ClearCurve1Btn.Size = new System.Drawing.Size(187, 26);
+            this.ClearCurve1Btn.TabIndex = 1;
+            this.ClearCurve1Btn.Text = "Clear Curve1";
+            this.ClearCurve1Btn.UseVisualStyleBackColor = true;
+            this.ClearCurve1Btn.Click += new System.EventHandler(this.ClearCurve1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.ClearCurve2Btn);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(198, 298);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Curve2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number:";
+            // 
+            // ClearCurve2Btn
+            // 
+            this.ClearCurve2Btn.Location = new System.Drawing.Point(3, 5);
+            this.ClearCurve2Btn.Name = "ClearCurve2Btn";
+            this.ClearCurve2Btn.Size = new System.Drawing.Size(187, 26);
+            this.ClearCurve2Btn.TabIndex = 2;
+            this.ClearCurve2Btn.Text = "Clear Curve2";
+            this.ClearCurve2Btn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(695, 448);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -117,20 +167,27 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button ClearButton;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ClearCurve1Btn;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ClearCurve2Btn;
     }
 }
 
--- numm/Form1.cs
+++ numm/Form1.cs
@@ -9,18 +9,25 @@ using System.Windows.Forms;
 
 namespace numm
 {
-
+    
 
     public partial class Form1 : Form
     {
 
         Graphics G;
-        Bezier B = new Bezier();
-        float h = 0.01f; // Resolution of each point
-                       
+        Bezier B1 = new Bezier();
+        Bezier B2 = new Bezier();
+
+        
+
         public Form1()
         {
             InitializeComponent();        
+            B2.PointCo = Color.Green;
+            B2.CurveCo = Color.Black;
+
+            label1.Text = B1.listP();
+            label2.Text = B2.listP();
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
@@ -29,64 +36,89 @@ namespace numm
             switch (e.Button)
             {
                 case MouseButtons.Left:
-                    B.addP(new PointF(e.X, e.Y));
-                    DrawP();
-                    label1.Text = B.listP();
+
+                    if(tabControl1.SelectedIndex ==0 )
+                        B1.addP(new PointF(e.X, e.Y));
+                    else
+                        B2.addP(new PointF(e.X, e.Y));
+
+                    G = pictureBox1.CreateGraphics();
                     
+                    label1.Text = B1.listP();
+                    label2.Text = B2.listP();
+                    DrawP(B1.CtrlPoint,B1.PointCo);
+                    DrawP(B2.CtrlPoint, B2.PointCo);
                     break;
                 case MouseButtons.Right:
-                    //Line_spline(B.CtrlPoint);
-                    Bezier_curve();
+                    if (tabControl1.SelectedIndex == 0)
+                    {
+                        ClearCurve1();
+                        Bezier_curve(B1);
+                    }
+                    else
+                    {
+                        ClearCurve1();
+                        Bezier_curve(B2);
+                    }
+                        
+
                     break;
             }
         }
 
-        void Bezier_curve()  // Draw the Bezier curve 
+        void Bezier_curve(Bezier b )  // Draw the Bezier curve 
         {
-            Bezier_calculate();
-
-            Pen pen_line = new Pen(Color.Blue);
-            for (int i = 0; i < (B.Point_Bezier.Count - 1); i++)
-                G.DrawLine(pen_line, B.Point_Bezier[i], B.Point_Bezier[i + 1]);
+            b.BezierCalculate();
+            Pen pen_line = new Pen(b.CurveCo);
+            for (int i = 0; i < (b.Point_Bezier.Count - 1); i++)
+                G.DrawLine(pen_line, b.Point_Bezier[i], b.Point_Bezier[i + 1]);
         }
-
-        void Bezier_calculate() // Calculate each point
+           
+        void DrawP(List<PointF> CP, Color c)
         {
-            PointF[,] Stack = new PointF[B.CtrlPoint.Count, B.CtrlPoint.Count];
-            for (int i = 0; i < B.CtrlPoint.Count; i++)
+            if (CP.Count <= 0)
+                return;
+                    
+            foreach (PointF p in CP)
             {
-                Stack[i, 0] = B.CtrlPoint[i];
-            }
-
-
-            for (float u = 0; u <= 1; u = u + h)
-            {
-                for (int j = 0; j < (B.CtrlPoint.Count-1); j++)
-                {
-                    for (int i = 0; i < (B.CtrlPoint.Count - 1); i++)
-                        Stack[i, j + 1] = new PointF((1 - u) * Stack[i + 1, j].X + u * Stack[i, j].X, (1 - u) * Stack[i + 1, j].Y + u * Stack[i, j].Y);
-                }
-                B.Point_Bezier.Add(Stack[0, (B.CtrlPoint.Count - 1)]);
-            }
-        }
-          
-        void DrawP()
-        {
-            G = pictureBox1.CreateGraphics();
-            foreach (PointF p in B.CtrlPoint)
-            {
-                System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
-                
-                G.FillEllipse(myBrush, new Rectangle((int)p.X,(int)p.Y,2,2));
-
+                System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(c);                
+                G.FillEllipse(myBrush, new Rectangle((int)p.X,(int)p.Y,4,4));
             }
         }
 
-        private void Clear(object sender, EventArgs e)
+        void ClearCurve1()
         {
             G.Clear(Color.White);
-            B.clear();
-            label1.Text = B.listP();
+            B1.clear();
+            DrawP(B2.CtrlPoint, B1.PointCo);
+            Bezier_curve(B1);
+            label1.Text = B1.listP();
+        }
+
+        void ClearCurve2()
+        {
+            G.Clear(Color.White);
+            B2.clear();
+            DrawP(B1.CtrlPoint, B1.PointCo);
+            Bezier_curve(B1);
+            label2.Text = B2.listP();
+        }
+
+        private void ClearCurve1(object sender, EventArgs e)
+        {
+            G.Clear(Color.White);
+            B1.clear();
+            DrawP(B2.CtrlPoint,B1.PointCo);
+            Bezier_curve(B1);
+            label1.Text = B1.listP();
+        }
+        private void ClearCurve2(object sender, EventArgs e)
+        {
+            G.Clear(Color.White);
+            B2.clear();
+            DrawP(B1.CtrlPoint, B1.PointCo);
+            Bezier_curve(B1);
+            label2.Text = B2.listP();
         }
 
         /*public struct PointD  //因為Point宣告是int，只好自己定義double
@@ -130,23 +162,34 @@ namespace numm
         public List<PointF> CtrlPoint = new List<PointF>();
         public List<PointF> Point_Bezier = new List<PointF>();
 
+       public Color PointCo = Color.Red;
+       public Color CurveCo = Color.Blue;
+
+        float h = 0.01f; // Resolution of each point
+
         public Bezier()
         {
 
         }
 
-        public Point P(double t)
+        public void BezierCalculate() // Calculate each point
         {
-            Point p = new Point(0, 0);
 
-            // 點賦值方法
-            p.X = 10;
-            p.Y = 20;
-            // 另一點賦值方法
-            p = new Point(30, 50);
-            return p;
+            PointF[,] Stack = new PointF[CtrlPoint.Count, CtrlPoint.Count];
+
+            for (int i = 0; i < CtrlPoint.Count; i++)
+                Stack[i, 0] = CtrlPoint[i];  
+
+            for (float u = 0; u <= 1; u = u + h)
+            {
+                for (int j = 0; j < (CtrlPoint.Count - 1); j++)
+                {
+                    for (int i = 0; i < (CtrlPoint.Count - 1); i++)
+                        Stack[i, j + 1] = new PointF((1 - u) * Stack[i + 1, j].X + u * Stack[i, j].X, (1 - u) * Stack[i + 1, j].Y + u * Stack[i, j].Y);
+                }
+                Point_Bezier.Add(Stack[0, (CtrlPoint.Count - 1)]);
+            }
         }
-
         public void addP(PointF a)
         {
             CtrlPoint.Add(a);
